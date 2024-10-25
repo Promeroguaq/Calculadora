32bcfecaa0b890caab7c1aea6b56c725d635c493
Promeroguaq <paolaromero1311@yahoo.com>
2024-10-25 11:32:36 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 const botones = document.querySelectorAll(".btn")
-const pantalla = document.querySelector(".pantalla")
+const pantalla = document.querySelector("#pantalla")
 
 console.log(botones)
 botones.forEach(boton => {
@@ -7,18 +7,28 @@ botones.forEach(boton => {
         const botonApretado = boton.textContent
         if (botonApretado === "AC") {
             pantalla.textContent = "0"
-
-        } else if (botonApretado === "del") {
+        } 
+        
+        else if (botonApretado === "del") {
             try { pantalla.textContent.length ==1 }{
                 pantalla.textContent = "0"
             } else{
                 pantalla.textContent = pantalla.textContent.slice(0,-1)
             }
-        } else {
-            if (pantalla.textContent === "0") {
-            pantalla.textContent = botonApretado
+        } else if (botonApretado == "="){
+            try{
+                pantalla.textContent =eval(pantalla.textContent)
+            }catch (error){
+                pantalla.textContent = "Error"
+            }
+
+        } else if(botonApretado == "%"){
+            try{
+                pantalla.textContent = eval(pantalla.textContent)*0.01
+            }catch (error){
+            pantalla.textContent = "Error!"
+            }
         }
         pantalla.textContent = pantalla.textContent + botonApretado
     }
     })
-})
